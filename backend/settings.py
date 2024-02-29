ad3831a87d275270e5399665d6c91a5b43f9ebe9
anup <anupmandal828109@gmail.com>
2024-02-29 23:06:22 +0530

    debug False

--- backend/settings.py
+++ backend/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3zem-twa*pdw1e5x#$y82dn*f4)sc90%a^a-qk=nc!ywk*xp+y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS = []
 ALLOWED_HOSTS = ['mywin11.onrender.com', '127.0.0.1']
