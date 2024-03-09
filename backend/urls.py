65e5d6b74b5546fd8c0423bbc2d9ec688e53d1b2
anup <anupmandal828109@gmail.com>
2024-03-10 03:19:49 +0530

    ok

--- backend/urls.py
+++ backend/urls.py
@@ -17,6 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from app.views import index
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
@@ -29,4 +33,4 @@ urlpatterns = [
     path('api/', include('app.urls')),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
